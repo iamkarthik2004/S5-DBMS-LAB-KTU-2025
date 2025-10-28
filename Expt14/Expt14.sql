0037e615dcba80ebd707cf5714031927e828bfac
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-10-28 10:46:58 +0530

    Update Expt14.sql

--- Expt14/Expt14.sql
+++ Expt14/Expt14.sql
@@ -48,3 +48,50 @@ select * from student_details;
 --Insert the tuple into the database.
 --Using function f1 calculate the grade
 --Using procedure p1, update the grade value for the tuple
+Create table student3(id int, name varchar(40), m1 int, m2 int, m3 int, total int, grade varchar(10));
+
+CREATE OR REPLACE FUNCTION f1(total_marks IN NUMBER)
+RETURN VARCHAR2
+AS
+grade VARCHAR2(2);
+BEGIN
+IF total_marks >= 240 THEN
+grade := 'A';
+ELSIF total_marks >= 180 THEN
+grade := 'B';
+ELSIF total_marks >= 120 THEN
+grade := 'C';
+ELSIF total_marks >= 60 THEN
+grade := 'D';
+ELSE
+grade := 'F';
+END IF;
+RETURN grade;
+END;
+/
+
+Create or replace procedure p1(stud_id in integer) as
+totalM number;
+st_grade varchar(5);
+begin
+select m1+m2+m3 into totalM from student3 where stud_id=id;
+st_grade := f1(totalM);
+
+update student3 set total = totalM, grade = st_grade where id=stud_id;
+commit;
+end;
+/
+
+declare
+v_id int := &id;
+v_name varchar(40) := &name;
+v_m1 int := &m1;
+v_m2 int := &m2;
+v_m3 int := &m3;
+begin
+  insert into student3(id, name, m1, m2, m3) values (v_id,v_name,v_m1,v_m2,v_m3);
+    p1(v_id);
+end;
+/
+
+select * from student3;
